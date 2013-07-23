db0c4ac84a8297ad1b1d1cbd59953bdb5c8c4253
Kaid <kaid@kaid.me>
2013-07-23 15:08:50 +0800

    moved model_methods to a separate file

--- lib/simple-credit.rb
+++ lib/simple-credit.rb
@@ -1,32 +1,9 @@
 require 'active_record'
 require 'simple-credit/credit'
+require 'simple-credit/credit_history'
+require 'simple-credit/model_methods'
 
 module SimpleCredit
-  class CreditHistory < ActiveRecord::Base
-    attr_accessible :model, :user, :delta, :scene, :what
-
-    belongs_to :user
-    belongs_to :model, polymorphic: true
-
-    default_scope lambda {order(id: :desc)}
-
-    after_save {|hist|
-      user   = hist.user
-      credit = user.credit
-
-      credit.update_attributes(value: user.credit_histories.sum(:delta))
-    }
-
-    def model=(obj)
-      self.to_id   = obj.id
-      self.to_type = obj.class.to_s
-    end
-
-    def model
-      self.to_type.constantize.find(self.to_id)
-    end
-  end
-
   module ModelMethods
     def self.included(base)
       base.send :extend, ClassMethods
---
+++ lib/simple-credit/model_methods.rb
@@ -0,0 +1,27 @@
+module SimpleCredit
+  module ModelMethods
+    def self.included(base)
+      base.send :extend, ClassMethods
+      base.has_many :credit_history,
+                    class_name: "SimpleCredit::CreditHistory",
+                    as: :model
+    end
+
+    module ClassMethods
+      def record_credit(options = {})
+        options[:on].each do |event|
+          send "after_#{event}" do |model|
+            user      = options[:user].call(model)
+            delta     = options[:delta].call(model)
+            what      = "#{event}_#{self.to_s}"
+            condition = options[:if].call(model)
+
+            user.credit_histories.create(model: model,
+                                         delta: delta,
+                                         what: what) if condition
+          end
+        end
+      end
+    end
+  end
+end
