0e8b72ecd85272ed77136ede7738f72a5acb8c06
Kaid <kaid@kaid.me>
2013-07-25 18:01:34 +0800

    refactored a bit

--- lib/simple-credit/credit_history.rb
+++ lib/simple-credit/credit_history.rb
@@ -8,15 +8,16 @@ module SimpleCredit
     default_scope lambda {order(id: :desc)}
 
     after_save {|hist|
-      user       = hist.user
-      credit     = user.credit
-      sum        = user.credit_histories.sum(:delta)
-      value      = sum >= 0 ? sum : 0
-      is_highest = value > user.highest_credit
+      value      = hist.sum >= 0 ? hist.sum : 0
+      is_highest = value > hist.user.highest_credit
 
-      credit.update_attributes(value: value)
-      credit.update_attributes(highest_value: value) if is_highest
+      hist.user.credit.update_attributes(value: value)
+      hist.user.credit.update_attributes(highest_value: value) if is_highest
     }
+
+    def sum
+      user.credit_histories.sum(:delta)
+    end
 
     def model=(obj)
       self.to_id   = obj.id
